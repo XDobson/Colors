9867f6404958b37f8f4bf8621bb1ee310b48d730
XDobson <xander.shaffer@stu.caverna.kyschools.us>
2022-03-14 10:43:52 -0500

    Flash colors go brrrr

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ let carrot = Math.floor(Math.random() * 256)
 let potato = Math.floor(Math.random() * 256)
 let lettuce = Math.floor(Math.random() * 256)
 let change = true;
+let stop = true;
+
+// This function i stole heartlessly from the internet
+function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
 function up() {
     if (carrot <= 255 && carrot >= 0) {
         carrot = carrot + (Math.floor(Math.random() * 7) - 3)
@@ -28,8 +35,8 @@ function up() {
     } else if (lettuce <= 0) {
         lettuce = lettuce + 2
     }
-
     document.body.style.background = "rgb(" + (carrot) + "," + (potato) + "," + (lettuce) + ",1)"
+    update()
     if (change == true) {
         return null
     }
@@ -40,9 +47,9 @@ function up() {
 function loop() {
     temp = setTimeout(up, 1);
     console.log("=======================");
-    console.log("Carrot:" + carrot);
-    console.log("Potato:" + potato);
-    console.log("Lettuce:" + lettuce);
+    console.log("(R) Carrot:" + carrot);
+    console.log("(G) Potato:" + potato);
+    console.log("(B) Lettuce:" + lettuce);
 }
 function jumpercable() {
     if (change == false) {
@@ -58,5 +65,40 @@ function jumpercable() {
     }
 }
 function zwoop() {
-    document.body.style.background = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")"
+    carrot = Math.floor(Math.random() * 256)
+    potato = Math.floor(Math.random() * 256)
+    lettuce = Math.floor(Math.random() * 256)
+    document.body.style.background = "rgb(" + carrot + "," + potato + "," + lettuce + ")"
+    update()
+    if (stop == false) {
+        restart()
+    }
 }
+function update() {
+    document.getElementById("red").innerText = carrot
+    document.getElementById("green").innerText = potato
+    document.getElementById("blue").innerText = lettuce
+    document.getElementById("hexcode").innerText = rgbToHex(carrot, potato, lettuce)
+}
+
+function automatic() {
+    zwoop()
+}
+function restart(){
+    temp = setTimeout(automatic, 1);
+}
+function jumpercable2(){
+    if (stop == false) {
+        stop = true
+        document.getElementById("randomize").style.background = "rgb(255,50,50)"
+    } else {
+        stop = false
+        document.getElementById("randomize").style.background = "rgb(50,255,50)"
+        carrot = Math.floor(Math.random() * 256)
+        potato = Math.floor(Math.random() * 256)
+        lettuce = Math.floor(Math.random() * 256)
+        restart()
+    }
+}
+
+alert("Website contains fast flashing colors.")
